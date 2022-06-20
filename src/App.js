9821400152aa1e6d0dd75828c577dae8f386480e
Samuel Reed <samuelreed01@gmail.com>
2022-06-20 15:14:56 +0700

    fix layout, remove popup import

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React, { useEffect, useState } from "react";
 import Catalog from "./components/Catalog";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Popup, { usePopup } from "./components/Popup/Popup";
 import Toast from "./components/Toast/Toast";
 
 function App() {
     const [location, setLocation] = useState("");
-    const { show, hide } = usePopup("New Title", "New Contents");
 
     useEffect(() => {
         setLocation(window.location.pathname.slice(1));
